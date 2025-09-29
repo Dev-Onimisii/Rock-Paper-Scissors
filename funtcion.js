3b02f545bcc952ed9a7ab4a9670e38d1f974d644
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2025-09-29 10:31:21 +0100

    Pushing the stying file

---
+++ funtcion.js
@@ -0,0 +1,81 @@
+const choiceRock = document.querySelector("#rock");
+const choicePaper = document.querySelector("#paper");
+const choiceScissors = document.querySelector("#scissors");
+const playerChoice = document.querySelector(".playerChoice");
+
+const playerScore = document.querySelector(".playerscore");
+const computerScore = document.querySelector(".computerscore");
+
+const computerChoice = document.querySelector(".computerChoice");
+const finalStatment = document.querySelector(".printout");
+
+let choices = ["rock", "paper", "scissors"];
+
+let cScore = 0;
+let pScore = 0;
+
+choiceRock.addEventListener("click", () => {
+  let randomChoice = choices[Math.floor(Math.random() * 3)];
+
+  if (randomChoice === "rock") {
+    playerChoice.textContent = "Rock";
+    computerChoice.textContent = "Rock";
+    finalStatment.textContent = "Draw!";
+  } else if (randomChoice === "scissors") {
+    playerChoice.textContent = "Rock";
+    computerChoice.textContent = "Scissors";
+    finalStatment.textContent = "You win!";
+    pScore++;
+    playerScore.textContent = pScore;
+  } else {
+    playerChoice.textContent = "Rock";
+    computerChoice.textContent = "Paper";
+    finalStatment.textContent = "You lose!";
+    cScore++;
+    computerScore.textContent = cScore;
+  }
+});
+
+choicePaper.addEventListener("click", () => {
+  let randomChoice = choices[Math.floor(Math.random() * 3)];
+
+  if (randomChoice === "rock") {
+    playerChoice.textContent = "Paper";
+    computerChoice.textContent = "Rock";
+    finalStatment.textContent = "You win!";
+    pScore++;
+    playerScore.textContent = pScore;
+  } else if (randomChoice === "scissors") {
+    playerChoice.textContent = "Paper";
+    computerChoice.textContent = "Scissors";
+    finalStatment.textContent = "You lose!";
+    cScore++;
+    computerScore.textContent = cScore;
+  } else {
+    playerChoice.textContent = "Paper";
+    computerChoice.textContent = "Paper";
+    finalStatment.textContent = "Draw!";
+  }
+});
+
+choiceScissors.addEventListener("click", () => {
+  let randomChoice = choices[Math.floor(Math.random() * 3)];
+
+  if (randomChoice === "rock") {
+    playerChoice.textContent = "Scissors";
+    computerChoice.textContent = "Rock";
+    finalStatment.textContent = "You lose!";
+    cScore++;
+    computerScore.textContent = cScore;
+  } else if (randomChoice === "scissors") {
+    playerChoice.textContent = "Scissors";
+    computerChoice.textContent = "Scissors";
+    finalStatment.textContent = "Draw!";
+  } else {
+    playerChoice.textContent = "Scissors";
+    computerChoice.textContent = "Paper";
+    finalStatment.textContent = "You win!";
+    pScore++;
+    playerScore.textContent = pScore;
+  }
+});
